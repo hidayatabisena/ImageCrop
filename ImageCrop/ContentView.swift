a74abbc816497d44de9a77c2634c247f4f364152
Han Sena <abisena@outlook.com>
2024-05-19 20:26:07 +0700

    Initial Commit

---
+++ ImageCrop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageCrop
+//
+//  Created by Hidayat Abisena on 19/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageCrop/ImageCropApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCropApp.swift
+//  ImageCrop
+//
+//  Created by Hidayat Abisena on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageCropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
